8ce7931bedfa14f9f98dbbf44cc6589126cbc742
Conor Patrick Mc Gavin <cpmg1@csg20-20.ucc.ie>
2022-03-27 14:17:40 +0100

    bash script updating results

--- run_instances.sh
+++ run_instances.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for instance in "'instances/random-32-32-20/agents_experiment/16/*'" "'instances/random-32-32-20/agents_experiment/24/*'" "'instances/random-32-32-20/agents_experiment/32/*'" "'instances/random-32-32-20/agents_experiment/40/*'"; do
+for instance in '"instances/random-32-32-20/agents_experiment/16/*"' '"instances/random-32-32-20/agents_experiment/24/*"' '"instances/random-32-32-20/agents_experiment/32/*"' '"instances/random-32-32-20/agents_experiment/40/*"'; do
 python3 run_experiments.py --instance $instance --solver Prioritized --replanner prioritized
 done;
 git add .
